5b787b9eaca2f9af141113a3f7310a174e3e11fe
Amanuel Ghebreamlak Ghebremedhin <amanuelghe84@gmail.com>
2025-11-06 20:43:25 +0100

    initial commit

---
+++ app/core/logging.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(level=logging.WARNING)
+
+logging.debug("Debug message")
+logging.info("Info message")
+logging.warning("Warning message")
+logging.error("Error message")
+logging.critical("Critical message")
+
