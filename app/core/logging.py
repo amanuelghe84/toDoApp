5922b2983d59f8d789994a4060e947de4127179c
Amanuel Ghebreamlak Ghebremedhin <amanuelghe84@gmail.com>
2025-11-08 11:39:37 +0100

    initial commit

--- app/core/logging.py
+++ app/core/logging.py
@@ -3,9 +3,12 @@ import json
 import logging
 import sys
 from io import StringIO
+from typing import Any
 
-logging.basicConfig(level=logging.WARNING,
-                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+from app.core.config import settings
+
+#logging.basicConfig(level=logging.WARNING,
+            # format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # Log messages at different levels
 logging.debug("Debug message")
@@ -14,49 +17,6 @@ logging.warning("Warning message")
 logging.error("Error message")
 logging.critical("Critical message")
 
-# text format
-# create logger
-logger = logging.getLogger(__name__)
-
-logger.setLevel(logging.DEBUG)
-
-# console handler
-console_handler = logging.StreamHandler()
-
-# Text formatter
-text_formatter = logging.Formatter(
-    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-)
-console_handler.setFormatter(text_formatter )
-
-# Add handler
-logger.addHandler(console_handler)
-
-# Log message
-logger = logging.info("This is a text log example")
-
-"""
-1. create logger | 1. add name  2. set level
-
-    # get logger with name set
-    logger = logging.getLogger(__name__)
-    # set logger level
-    logger.setLevel(logging.DEBUG)
-
-
-2. create log handler or output mechanism
-
-    # set log terminal or console handler
-    console_log_handler = logging.StreamHandler()
-    #create log formatter
-    custom_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelName)s - %(message)s)
-    # set the formatter to be our custom formatter `console_log_formatter`
-    console_log_handler.setFormatter(formatter)
-
-3. add console log output handler to our logger
-
-
-"""
 
 class JsonFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
@@ -70,8 +30,22 @@ class JsonFormatter(logging.Formatter):
 
 
 
-logger = logging.getLogger("json_example")
+logger = logging.getLogger("json_Example")
 logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+json_formatter = JsonFormatter()
+console_handler.setFormatter(json_formatter)
+
+file_handler = logging.FileHandler("app.log")
+file_handler.setFormatter(json_formatter)
+
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logging.Logger.info("This is a JSON example")
+
 
 
 
